e505a8f2d4c970d77aa19c661100ca29ea31cd86
Jesús Muñoz a.k.a. "omiK <munozcastro.jj@gmail.com>
2018-11-02 20:56:17 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+/*
+HOW TO FORM NUMBERS:
+
+- When a symbol appears after a larger (or equal) symbol it is added
+
+VI = V + I = 5 + 1 = 6
+LXX = L + X + X = 50 + 10 + 10 = 70
+
+- But if the symbol appears before a larger symbol it is subtracted
+
+IV = V − I = 5 − 1 = 4
+IX = X − I = 10 − 1 = 9
+
+- Do not use a symbol more than three times in a row
+
+HOW TO CONVERT TO ROMAN (iex. 1984):
+
+- Break 1984 into 1000, 900, 80 and 4, then do each conversion
+
+1000 = M
+900 = CM
+80 = LXXX
+4 = IV
+
+1000 + 900 + 80 + 4 = 1984, so 1984 = MCMLXXXIV 
+*/
+
+function convertToRoman(num) {
+
+  let romanDict = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+  let returnRoman = '';
+
+  for (var i in romanDict) {
+
+    while (num >= romanDict[i]) {
+
+      console.log(num);
+      returnRoman = returnRoman + i;
+      num = num - romanDict[i];
+      console.log(returnRoman);
+    }
+  }
+  
+  return returnRoman;
+}
+
+convertToRoman(36);
+// 36 : 30 XXXVI
